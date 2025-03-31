9118205a95a7fff02f84c2e3a1022a7c85bb91c7
anythinglord <sjsalcedoa@unal.edu.co>
2025-03-31 17:13:28 -0500

    nested & dynamic routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+export default async function ProductDetail({ params, }: {
+    params: Promise<{ productId: string }>
+}) {
+    const productId = (await params).productId
+    return <h1>Details about product {productId}</h1>
+}
